52c542358e6e08ef8e3e57cab21d0e32fbe9e4ee
iffysravioli <111910162+iffysravioli@users.noreply.github.com>
2023-05-02 16:27:35 -0400

    Update mycode.py

--- mycode.py
+++ mycode.py
@@ -1,1 +1,30 @@
-#test
+#GITHUB - iffysravioli 
+#https://github.com/iffysravioli/unixgit
+
+import sys 
+
+string = sys.stdin
+lst = []
+cipher = sys.stdin
+message = ""
+num = 5 
+count = 0
+new_message = ""
+string = string.upper()
+for letter in range(0,len(string)):
+  if 65 <= ord(string(letter)) <= 90:
+    char = ord(string(letter)) + cipher
+    if char > 90:
+      char -= 26
+      code = chr(char)
+      message += code
+for new_letter in range(0, len(message), num):
+  lst.append(message[new_letter + num ])
+for word in lst:
+  new_message += word 
+  new_message += " "
+  count += 1
+  if count == 10:
+    new_letter += "\n"
+    count = 0 
+  print(new_message)
