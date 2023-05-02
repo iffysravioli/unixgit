27947092c516732831e89c5b06c8dea3d34c8877
iffysravioli <111910162+iffysravioli@users.noreply.github.com>
2023-05-02 16:35:57 -0400

    Update mycode.py

--- mycode.py
+++ mycode.py
@@ -1,30 +1,29 @@
 #GITHUB - iffysravioli 
 #https://github.com/iffysravioli/unixgit
-
 import sys 
 
-string = sys.stdin
+string = input()
 lst = []
-cipher = sys.stdin
+cipher = int(input())
 message = ""
 num = 5 
 count = 0
 new_message = ""
-string = string.upper()
+string = str(string).upper()
 for letter in range(0,len(string)):
-  if 65 <= ord(string(letter)) <= 90:
-    char = ord(string(letter)) + cipher
+  if 65 <= ord(string[letter]) <= 90:
+    char = ord(string[letter]) + cipher
     if char > 90:
       char -= 26
-      code = chr(char)
-      message += code
+    code = chr(char)
+    message += code
 for new_letter in range(0, len(message), num):
-  lst.append(message[new_letter + num ])
+  lst.append(message[new_letter:new_letter + num])
 for word in lst:
   new_message += word 
   new_message += " "
   count += 1
   if count == 10:
-    new_letter += "\n"
+    new_message += "\n"
     count = 0 
-  print(new_message)
+print(new_message)
